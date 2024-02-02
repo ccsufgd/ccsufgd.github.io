2b70c31aea3da195a7176db44f338410f861dcd3
ccsufgd <158065935+ccsufgd@users.noreply.github.com>
2024-02-02 08:48:11 -0400

    Add files via upload

---
+++ versão/v1/areas.js
@@ -0,0 +1,47 @@
+const areasData = [{
+    faculdade: 'FACE',
+    area: 'CIÊNCIAS CONTÁBEIS',
+    pontosSorteados: [],
+    pontos: [
+      '1 - Teste de Recuperabilidade',
+      '2 - Combinação de Negócios: Fusão, Cisão e Incorporação',
+      '3 - Métodos de Avaliação de Investimento',
+      '4 - Consolidação das demonstrações contábeis',
+      '5 - CPC 32 - Tributos sobre o Lucro',
+      '6 - Diferenças entre Lucro Real e Lucro Presumido (abordagem prática)',
+      '7 - Mensuração, contabilização e evidenciação de Instrumentos Financeiros',
+      '8 - DRA - Demonstração do Resultado Abrangente',
+      '9 - DFC - Demonstrativo de Fluxo de Caixa',
+      '10 - Orçamento Público'
+    ]
+  },
+  {
+    faculdade: 'FACE',
+    area: 'ECONOMIA E FINANÇAS',
+    pontosSorteados: [],
+    pontos: [
+      '1 - Avaliação de Ações',
+      '2 - Risco, Retorno e Seleção de Carteiras: Modelo CAPM e Teoria de Markowitz',
+      '3 - Análise de Investimentos: VPL, TIR, Payback, Razão Custo-benefício e demais critérios',
+      '4 - Macroeconomia em economia aberta: taxas de câmbio, políticas monetária e fiscal',
+      '5 - Inflação, Desemprego e Curva de Phillips: evolução e a questão das expectativas',
+      '6 - O Modelo de Crescimento de Solow',
+      '7 - Política Monetária no Plano Real nas décadas de 1990 e 2000',
+      '8 - Teoria do Consumidor',
+      '9 - Teoria da Produção',
+      '10 - Estruturas de Mercado'
+    ]
+  },
+  {
+    faculdade: 'FACET',
+    area: 'COMPUTAÇÃO',
+    pontosSorteados: [],
+    pontos: [
+      '1 - Sistemas de produção contínuos e de eventos discretos',
+      '2 - Sensores, atuadores, controladores lógico-programáveis',
+      '3 - Técnicas inteligentes de planejamento e controle da produção',
+      '4 - Diodo semicondutor',
+      '5 - Transistores de junção bipolar e de efeito de campo'
+    ]
+  }
+]
--- versão/v3/script.js
+++ versão/v3/script.js
@@ -1,4 +1,4 @@
-﻿
+
 const areas = areasData;
 
 console.log(areas);
@@ -101,18 +101,14 @@ function realizarSorteio() {
 
     resultadoElement.innerHTML = `
       <hr>
-      <br>
-      <p class="area-title"><strong style="font-size: 1.3em;">${resultado.area} - ${resultado.faculdade}</strong></p>
+      <p class="area-title"><strong>${resultado.area} - ${resultado.faculdade}</strong></p>
       <p><strong>Prova Escrita:</strong></p>
 
       <p><mark>${resultado.pontoProvaEscrita}</mark></p>
       <p><strong>Prova Didática:</strong></p>
 
       <p><mark>${resultado.pontoProvaDidatica}</mark></p>
-      <br>
     `;
-
-    
 
     // Append the resultadoElement to the DOM or wherever you want to display it
     document.body.appendChild(resultadoElement);
@@ -121,22 +117,12 @@ function realizarSorteio() {
 
   });
 
-
-  const dataAtual = new Date();
-
-  // Formata a data e a hora (você pode ajustar o formato conforme necessário)
-  const formatoDataHora = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
-  const dataHoraFormatada = dataAtual.toLocaleDateString('pt-BR', formatoDataHora);
-  infoContainer.innerHTML += `<div class="alert alert-info" role="alert">
-     <p class="area-title">${dataHoraFormatada}</p>
-  </div><br>`;
-
   console.log('Sorteio Realizado');
   console.log(resultadosSorteio);
   sorteioRealizado = true;
   print = false;
   
-  exibirAlerta('success', 'Sorteio Realizado!');
+  exibirAlerta('success', 'Sorteio Realizado com Sucesso!');
   showButtons();
 }
 
@@ -146,10 +132,16 @@ function sortearPonto(pontos, pontoExcluido, area) {
   
   const infoContainer = document.getElementById('infoContainer');
 
+  if (!infoContainer) {
+    console.error('Elemento infoContainer não encontrado.');
+    return;
+  }
+
   // Adicionar texto apenas se ainda não foi adicionado
   if (!print) {
     infoContainer.innerHTML += '<br><hr>';
     infoContainer.innerHTML += '<div class="alert alert-success" role="alert"><p class="area-title"><strong>Arquivo de Logs</strong></p></div>';
+    
     infoContainer.innerHTML += '<br>';
     print = true;
   }
@@ -159,13 +151,7 @@ function sortearPonto(pontos, pontoExcluido, area) {
   if (!pontoExcluido) {
     console.log(`Sorteio para a área: ${area.area} (Faculdade: ${area.faculdade})`);
     console.log('Os pontos disponíveis para sorteio da Prova Escrita', pontos);
-    infoContainer.innerHTML += `
-    <div class="alert alert-dark" role="alert" style="text-align: center;">
-    <br>
-      <p class="area-title">
-      <strong>${area.area}</strong> (Faculdade: ${area.faculdade})</p>
-      <br>
-    </div>`;
+    infoContainer.innerHTML += `<p class="area-title">Sorteio para a área: <strong>${area.area}</strong> (Faculdade: ${area.faculdade})</p>`;
     infoContainer.innerHTML += `<p><strong>Os pontos disponíveis para sorteio da Prova Escrita:</strong> <br>${pontos.join('<br> ')}</p>`;
     
   }
@@ -178,18 +164,9 @@ function sortearPonto(pontos, pontoExcluido, area) {
     console.log('Ponto excluído para sorteio da Prova Didática::', pontoExcluido);
     console.log('Os pontos disponíveis após exclusão são:', pontosDisponiveis);
 
-    infoContainer.innerHTML += ``;
-    infoContainer.innerHTML += `
-    <div class="alert alert-success" role="alert" style="text-align: center;">
-    <p class="area-title">Ponto sorteado <strong>Prova Escrita:</strong><br>
-    <strong>${pontoExcluido}</strong>
-    </div>`;
-    infoContainer.innerHTML += `
-    <div class="alert alert-danger" role="alert" style="text-align: center;">
-    <p class="area-title">Ponto excluído para sorteio da <strong>Prova Didática:</strong><br>
-    <strong><s>${pontoExcluido}</s></strong>
-    </div>`;
-    infoContainer.innerHTML += `<p><strong>Os pontos disponíveis para sorteio da Prova Didática:</strong><br>${pontosDisponiveis.join('<br> ')}</p>`;
+    infoContainer.innerHTML += `<p class="area-title">Ponto sorteado Prova Escrita:<br><mark style="color: black; background-color:#adffb7">${pontoExcluido}</mark></p>`;
+    infoContainer.innerHTML += `<p class="area-title">Ponto excluído para sorteio da Prova Didática:<br> <mark style="color: black; background-color:#ffa3a3">${pontoExcluido}</mark></p>`;
+    infoContainer.innerHTML += `<p><strong>Os pontos disponíveis após exclusão:</strong><br>${pontosDisponiveis.join('<br> ')}</p>`;
   }
 
   const randomBytes = new Uint8Array(1);
@@ -202,37 +179,9 @@ function sortearPonto(pontos, pontoExcluido, area) {
     console.log('Ponto sorteado Prova Didática:', pontoSorteado);
     console.log('Finalizado');
 
-    infoContainer.innerHTML += `
-    <div class="alert alert-success" role="alert" style="text-align: center;">
-    <p class="area-title">Ponto sorteado para <strong>Prova Didática:</strong><br>
-    <strong>${pontoSorteado}</strong>
-    </div>`;
-
-    infoContainer.innerHTML += `
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            
-            <th scope="col"><strong>Resultado Sorteio - ${area.area}</strong> (Faculdade: ${area.faculdade})</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td><strong>Prova Escrita</strong>: ${pontoExcluido}</td>
-          </tr>
-          <tr>
-            <td><strong>Prova Didática:</strong> ${pontoSorteado}</td>
-          </tr>
-        </tbody>
-      </table>
-    `;
-
-
-    infoContainer.innerHTML += `<div class="alert alert-info" role="alert">
-   <p class="area-title">Finalizado!</p></div><br><hr></hr>
-`;
-
-
+    infoContainer.innerHTML += `<p class="area-title">Ponto sorteado Prova Didática: <br><mark style="color: black; background-color:#adffb7"> ${pontoSorteado}</mark></p>`;
+    infoContainer.innerHTML += `<p class="area-title">Finalizado!</p><hr></hr>`;
+    
   }
 
   return pontoSorteado;
@@ -285,15 +234,13 @@ function viewResults() {
     resultadoElement.className = 'resultado-area';
     resultadoElement.innerHTML = `
       <hr>
-      <br>
-      <p class="area-title"><strong style="font-size: 1.3em;">${resultado.area} - ${resultado.faculdade}</strong></p>
+      <p class="area-title"><strong>${resultado.area} - ${resultado.faculdade}</strong></p>
       <p><strong>Prova Escrita:</strong></p>
 
       <p><mark>${resultado.pontoProvaEscrita}</mark></p>
       <p><strong>Prova Didática:</strong></p>
 
       <p><mark>${resultado.pontoProvaDidatica}</mark></p>
-      <br>
     `;
     sorteioContainer.appendChild(resultadoElement);
     
@@ -324,53 +271,47 @@ function saveResults() {
     csvContent += csvRow;
   });
 
-  // Prepare text file content for results
-  let textContentResults = "Resultados do Sorteio\n\n";
+  // Prepare text file content
+  let textContent = "Resultados do Sorteio\n\n";
   resultadosSorteio.forEach(resultado => {
-    textContentResults += `Área: ${resultado.area} - ${resultado.faculdade}\n`;
-    textContentResults += `Prova Escrita:   ${resultado.pontoProvaEscrita}\n`; // Adjust spacing
-    textContentResults += `Prova Didática:  ${resultado.pontoProvaDidatica}\n\n`; // Adjust spacing
+    textContent += `Área: ${resultado.area} - ${resultado.faculdade}\n`;
+    textContent += `Prova Escrita:   ${resultado.pontoProvaEscrita}\n`; // Adjust spacing
+    textContent += `Prova Didática:  ${resultado.pontoProvaDidatica}\n\n`; // Adjust spacing
   });
 
   // Manually add newline characters for better compatibility
-  textContentResults = textContentResults.replace(/\n/g, '\r\n');
+  textContent = textContent.replace(/\n/g, '\r\n');
 
-  // Prepare text file content for infoContainer
-  const infoContainerContent = document.getElementById('infoContainer').innerText;
-  let textContentInfoContainer = "Conteúdo do infoContainer\n\n";
-  textContentInfoContainer += infoContainerContent;
-  // Manually add newline characters for better compatibility
-  textContentInfoContainer = textContentInfoContainer.replace(/\n/g, '\r\n');
-
-  // Create Blobs with the CSV and text content
+  // Create a Blob with the CSV content
   const csvBlob = new Blob([csvContent], { type: 'text/csv' });
-  const textBlobResults = new Blob([textContentResults], { type: 'text/plain' });
-  const textBlobInfoContainer = new Blob([textContentInfoContainer], { type: 'text/plain' });
 
-  // Download CSV file
-  downloadFile(csvBlob, 'resultados_sorteio.csv');
+  // Create a Blob with the text content
+  const textBlob = new Blob([textContent], { type: 'text/plain' });
 
-  // Download text file for results
-  downloadFile(textBlobResults, 'resultados_sorteio.txt');
-
-  // Download text file for infoContainer
-  downloadFile(textBlobInfoContainer, 'logs.txt');
-}
-
-function downloadFile(blob, fileName) {
-  // Create a download link for the file
-  const fileLink = document.createElement('a');
-  fileLink.href = URL.createObjectURL(blob);
-  fileLink.download = fileName;
+  // Create a download link for the CSV file
+  const csvLink = document.createElement('a');
+  csvLink.href = URL.createObjectURL(csvBlob);
+  csvLink.download = 'resultados_sorteio.csv';
 
   // Append the link to the body and trigger a click
-  document.body.appendChild(fileLink);
-  fileLink.click();
+  document.body.appendChild(csvLink);
+  csvLink.click();
 
   // Remove the link from the body
-  document.body.removeChild(fileLink);
-}
+  document.body.removeChild(csvLink);
 
+  // Create a download link for the text file
+  const textLink = document.createElement('a');
+  textLink.href = URL.createObjectURL(textBlob);
+  textLink.download = 'resultados_sorteio.txt';
+
+  // Append the link to the body and trigger a click
+  document.body.appendChild(textLink);
+  textLink.click();
+
+  // Remove the link from the body
+  document.body.removeChild(textLink);
+}
 
 
 
@@ -416,4 +357,3 @@ function ocultarAlerta() {
   const customAlert = document.getElementById('customAlert');
   customAlert.style.display = 'none';
 }
-
