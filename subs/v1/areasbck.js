b76f4146a20bea458f85e9b4bd68113986ba864d
ccsufgd <158065935+ccsufgd@users.noreply.github.com>
2024-03-07 12:37:14 -0400

    Create areasbck.js

---
+++ subs/v1/areasbck.js
@@ -0,0 +1,421 @@
+const areasData = [{
+    faculdade: 'EaD',
+    area: 'Libras / Linguística',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Aspectos linguísticos da Libras: Língua Brasileira de Sinais.',
+    '2 - O sujeito surdo: conceitos, cultura e relação histórica da surdez com a língua de sinais.',
+    '3 - Desafios do ensino de Libras no ensino superior: presencial, híbrido e EaD.',
+    '4 - Linguística aplicada ao ensino da língua brasileira de sinais: desafios da didática na educação de surdos.',
+    '5 - Sistemas de escrita de Línguas de Sinais.'
+  ]
+  },
+  {
+    faculdade: 'FACE',
+    area: 'Administração Geral / Gestão de Pessoas',
+    pontosSorteados: [],
+    pontos: [
+    '1 - A Evolução do Pensamento em Administração',
+    '2 - Áreas Funcionais da Administração',
+    '3 - Gestão por Competências',
+    '4 - Gestão Estratégica de Pessoas',
+    '5 - Gestão de Qualidade de Vida no Trabalho'
+  ]
+  },
+  {
+    faculdade: 'FACE',
+    area: 'Administração Geral / Gestão de Materiais e Logística',
+    pontosSorteados: [],
+    pontos: [
+    '1 - A Evolução do Pensamento em Administração',
+    '2 - Áreas Funcionais da Administração',
+    '3 - Administração Patrimonial',
+    '4 - Gestão de Estoques',
+    '5 - Normalização e Classificação de Materiais'
+  ]
+  },
+  {
+    faculdade: 'FACE',
+    area: ' Economia / Teoria Econômica',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Método de Equivalência Patrimonial',
+    '2 - Combinação de negócios',
+    '3 - Instrumentos Financeiros',
+    '4 - Demonstrações consolidadas',
+    '5 - Valor Justo considerando Ativos Biológicos'
+  ]
+  },
+  {
+    faculdade: 'FACET',
+    area: 'Ensino de Matemática',
+    pontosSorteados: [],
+    pontos: [
+    '1 - A formação de professores de Matemática: desafios e perspectivas.',
+    '2 - Tecnologias digitais na formação de professores de matemática e suas relações com a Educação Básica.',
+    '3 - Estágios supervisionados na formação inicial de professores de Matemática.',
+    '4 - O Tratamento da Informação na formação de professores de matemática e suas relações com a Educação Básica.',
+    '5 - História da Matemática na formação de professores de matemática e suas relações com a Educação Básica.'
+  ]
+  },
+  {
+    faculdade: 'FACET',
+    area: 'Ciência da Computação / Algoritmos e Teoria da Computação',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Vetores, matrizes e registros',
+    '2 - Algoritmos de Ordenação',
+    '3 - Operações e relações em conjuntos',
+    '4 - Pilhas e filas',
+    '5 - Autômatos finitos'
+  ]
+  },
+  {
+    faculdade: 'FACET',
+    area: 'Estatística',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Estatística Descritiva.',
+    '2 - Distribuições de probabilidade.',
+    '3 - Teoria da Estimação: Intervalos de Confiança.',
+    '4 - Testes de hipóteses.',
+    '5 - Correlação e Regressão Linear.'
+  ]
+  },
+  {
+    faculdade: 'FADIR',
+    area: 'Direito Penal e Direito Processual Penal',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Princípios do Direito Penal',
+    '2 - Teoria do Crime',
+    '3 - Princípios do Processo Penal',
+    '4 - Medidas Cautelares de natureza pessoal no processo penal',
+    '5 - Nulidades no Processo Penal'
+  ]
+  },
+  {
+    faculdade: 'FADIR',
+    area: 'Política Internacional Contemporânea',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Cooperação Internacional e Governança Global;',
+    '2 - Organismos Multilaterais e Política Global;',
+    '3 - Soberania, Conflitos Internacionais e Intervenções Humanitárias;',
+    '4 - Reestruturação Produtiva e Globalização Econômica',
+    '5 - Economia Política Internacional, Desenvolvimento e Dependência'
+  ]
+  },
+  {
+    faculdade: 'FAED',
+    area: 'Pedagogia / Política e Gestão da Educação',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Políticas educacionais para a Educação Básica.',
+    '2 - Organização e gestão da educação escolar.',
+    '3 - Gestão educacional e escolar.',
+    '4 - Planejamento Educacional Brasileiro.',
+    '5 - Medidas e avaliação da educação'
+  ]
+  },
+  {
+    faculdade: 'FAED',
+    area: 'Pedagogia / Educação Especial',
+    pontosSorteados: [],
+    pontos: [
+    '1 - História da Educação Especial.',
+    '2 - Políticas públicas de Educação Especial.',
+    '3 - Inclusão escolar: raízes históricas, definição e características.',
+    '4 - O atendimento educacional especializado voltado ao aluno público alvo da Educação Especial.',
+    '5 - Práticas pedagógicas inclusivas'
+  ]
+  },
+  {
+    faculdade: 'FAED',
+    area: 'Pedagogia / Formação de Professores / Currículo e Ensino da Matemática e Estágio',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Recursos e materiais manipuláveis para o ensino da Matemática.',
+    '2 - O ensino da Matemática na Educação Infantil',
+    '3 - O ensino do sistema de numeração nos anos iniciais do Ensino Fundamental',
+    '4 - As Operações Matemáticas nos Anos Iniciais do Ensino Fundamental',
+    '5 - Práticas pedagógicas de Matemática e Estágio Supervisionados.'
+  ]
+  },
+  {
+    faculdade: 'FAED',
+    area: 'Pedagogia / Formação de Professores(as)',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Didática, formação de professores e os desafios do Estágio Supervisionado.',
+    '2 - Teoria e prática do currículo.',
+    '3 - Tendências pedagógicas contemporâneas.',
+    '4 - Planejamento e Avaliação da aprendizagem.',
+    '5 - Planejamento das práticas pedagógicas.'
+  ]
+  },
+  {
+    faculdade: 'FAEN',
+    area: 'Engenharia do Trabalho e Desenho Técnico',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Metodologia de análise ergonômica do trabalho.',
+    '2 - Convenção 169 da Organização Internacional do Trabalho na América do Sul.',
+    '3 - Fundamentos para representações gráficas.',
+    '4 - Introdução e uso de software para desenho técnico 2D e 3D.',
+    '5 - Planejamento do arranjo físico da produção.'
+  ]
+  },
+  {
+    faculdade: 'FAEN',
+    area: 'Gestão de Projetos e Pesquisa Operacional',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Viabilidade econômica em projetos.',
+    '2 - Introdução à pesquisa operacional e à modelagem de sistemas.',
+    '3 - Planejamento e controle de projetos: escopo, cronograma e orçamento.',
+    '4 - Teoria da decisão para estratégias de produção.',
+    '5 - Luminotécnica.'
+  ]
+  },
+  {
+    faculdade: 'FAEN',
+    area: 'Engenharia de Alimentos / Ciência e Tecnologia de Alimentos',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Parâmetros de qualidade da carne fresca.',
+    '2 - Processamento de carnes e derivados.',
+    '3 - Ferramentas da Qualidade.',
+    '4 - Fundamentos para Representações Gráficas.',
+    '5 - Métodos sensoriais discriminativos.'
+  ]
+  },
+  {
+    faculdade: 'FAEN',
+    area: 'Sistemas de Produção',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Gestão estratégica do desempenho das operações;',
+    '2 - Estudo de tempos;',
+    '3 - Caracterização do sistema de operações de serviços;',
+    '4 - Gestão agroindustrial: conceitos e aplicações;',
+    '5 - Fundamentos da representação digital e modelagem.'
+  ]
+  },
+  {
+    faculdade: 'FAEN',
+    area: 'Engenharia de Produto e Gestão da Qualidade',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Modelo de referência para gestão do desenvolvimento de produto.',
+    '2 - Projeto informacional e conceitual de desenvolvimento de produto.',
+    '3 - Ferramentas e abordagens para melhoria da qualidade.',
+    '4 - Modelos de referência para o gerenciamento da qualidade.',
+    '5 - Análise de riscos e falhas em produtos e processos.'
+  ]
+  },
+  {
+    faculdade: 'FAIND',
+    area: 'Ciências Humanas / Ensino Intercultural em Ciências Humanas',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Ensino de Ciências Humanas na escola de educação básica Guarani e Kaiowa numa perspectiva Intercultural e decolonial',
+    '2 - A territorialidade Guarani e Kaiowa e o Ensino de Ciências Humanas na escola indígena',
+    '3 - A interculturalidade e a interdisciplinaridade no ensino de Ciências Humanas na escola Guarani e Kaiowa',
+    '4 - A pedagogia da alternância na formação de docentes Guarani e Kaiowa',
+    '5 - O uso das línguas Guarani e Kaiowa no ensino de Ciências Humanas'
+  ]
+  },
+  {
+    faculdade: 'FALE',
+    area: 'Literatura',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Literatura Brasileira',
+    '2 - Literatura Comparada',
+    '3 - Literatura Portuguesa',
+    '4 - O Ensino de Literatura nas Escolas Públicas',
+    '5 - Teorias Literárias'
+  ]
+  },
+  {
+    faculdade: 'FALE',
+    area: 'Linguística e Língua Portuguesa',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Concepções teóricas de língua, linguagem, gramática e ensino de línguas',
+    '2 - Gêneros discursivos acadêmicos',
+    '3 - Língua e discurso',
+    '4 - Linguagem, sociedade e escola: norma, uso, variação e preconceito linguístico',
+    '5 - Texto e textualidade no ensino de língua portuguesa e nos gêneros discursivos'
+  ]
+  },
+  {
+    faculdade: 'FALE',
+    area: 'Processos de Criação e Pedagogia das Artes Cênicas',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Abordagens metodológicas do ensino de atuação.',
+    '2 - Treinamento físico: diretrizes para a composição do papel.',
+    '3 - A atuação teatral e suas relações com outras áreas: circo, dança e performance.',
+    '4 - Procedimentos metodológicos da pedagogia teatral contemporânea.',
+    '5 - Linguagem Teatral e Educação Infantil.'
+  ]
+  },
+  {
+    faculdade: 'FCA',
+    area: 'Engenharia de aquicultura / Aquicultura',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Cadeia produtiva, mercado e tecnologias disponíveis para peixes nativos do Brasil.',
+    '2 - Produção de vegetais aquáticos associados à aquicultura.',
+    '3 - Manejo de reprodução de peixes nativos.',
+    '4 - Manejo alimentar para peixes nativos.',
+    '5 - Produção de alimento vivo para larvicultura de peixes.'
+  ]
+  },
+  {
+    faculdade: 'FCA',
+    area: 'Agricultura Geral',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Propagação sexuada e assexuada de frutíferas;',
+    '2 - Princípios que regem a poda em frutíferas;',
+    '3 - Pós-colheita de frutas;',
+    '4 - Delineamentos experimentais aplicados na agricultura;',
+    '5 - Manejo integrado de doenças de plantas.'
+  ]
+  },
+  {
+    faculdade: 'FCA',
+    area: 'Solos ',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Minerais: conceito, propriedades, classificação; minerais primários e secundários;',
+    '2 - Atributos físicos do solo sob diferentes sistemas de manejo;',
+    '3 - Sistema Brasileiro de Classificação de Solos;',
+    '4 - Práticas de manejo solo para conservação do solo e da água em microbacias hidrográficas;',
+    '5 - Biota do solo como reguladora do ecossistema;'
+  ]
+  },
+  {
+    faculdade: 'FCA',
+    area: 'Eletrônica e Eletrotécnica',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Componentes dos circuitos eletrônicos;',
+    '2 - Circuitos de acionamento e controle de potência DC;',
+    '3 - Circuitos de acionamento e controle de potência AC;',
+    '4 - Controle de fator de potência;',
+    '5 - Geradores e motores de corrente alternada.'
+  ]
+  },
+  {
+    faculdade: 'FCH',
+    area: 'Ciências Sociais / Ciência Política',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Introdução à Política: definição e perspectivas.',
+    '2 - Teoria Política Moderna.',
+    '3 - Teoria Política Contemporânea.',
+    '4 - Democracia, representação e participação política.',
+    '5 - Política Brasileira.'
+  ]
+  },
+  {
+    faculdade: 'FCH',
+    area: 'Psicologia Escolar',
+    pontosSorteados: [],
+    pontos: [
+    '1 - A atuação do psicólogo na escola;',
+    '2 - Educação Especial e Educação inclusiva;',
+    '3 - Problemas de aprendizagem; A queixa escolar e a medicalização na escola;',
+    '4 - História do ensino e da escola; Psicologia Escolar;',
+    '5 - Psicologia do trabalho e as organizações; Saúde mental e trabalho.'
+  ]
+  },
+  {
+    faculdade: 'FCH',
+    area: 'Psicologia Clínica / Terapia Familiar Sistêmica',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Paradigma sistêmico e terapia familiar sistêmica;',
+    '2 - O campo da terapia familiar sistêmica: dos pioneiros aos contemporâneos;',
+    '3 - Avaliação do funcionamento familiar e psicoterapias;',
+    '4 - O ciclo de vida familiar;',
+    '5 - Problemas familiares e intervenção clínica.'
+  ]
+  },
+  {
+    faculdade: 'FCS',
+    area: 'Ginecologia e Obstetrícia',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Atenção e Assistência ao Pré-natal',
+    '2 - Avaliação da vitalidade fetal',
+    '3 - Assistência ao parto',
+    '4 - Contracepção',
+    '5 - Avaliação no sangramento uterino anormal'
+  ]
+  },
+  {
+    faculdade: 'FCS',
+    area: 'Ciências Morfofuncionais aplicadas à medicina',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Inflamação aguda',
+    '2 - Morte celular: apoptose e necrose',
+    '3 - Reparo de lesões: regeneração e cicatrização',
+    '4 - Tolerância Imunológica e autoimunidade',
+    '5 - Imunodeficiências primárias e adquiridas'
+  ]
+  },
+  {
+    faculdade: 'FCS',
+    area: 'Semiologia Médica',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Anamnese',
+    '2 - Dor',
+    '3 - Exame físico do sistema cardiovascular',
+    '4 - Dispneia',
+    '5 - Doenças da parede e da cavidade abdominal'
+  ]
+  },
+  {
+    faculdade: 'FCS',
+    area: 'Dietética',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Classificação NOVA e o Guia alimentar para a população brasileira.',
+    '2 - Avaliação do estado nutricional de praticantes de atividade física.',
+    '3 - Planejamento de intervenção nutricional para diferentes modalidades esportivas, em diferentes momentos da competição.',
+    '4 - Fisiologia da gestação.',
+    '5 - Avaliação do estado nutricional e planejamento dietético para gestantes.'
+  ]
+  },
+  {
+    faculdade: 'FCS',
+    area: 'Alimentação Coletiva',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Conceito de Alimentação Coletiva/ O papel do nutricionista na Alimentação Coletiva.',
+    '2 - UAN (unidades de Alimentação e Nutrição) e UPR (Unidades Produtoras de Refeições):',
+    '3 - conceitos; características; exigências legais para o seu funcionamento.',
+    '4 - Localização, layout, setores, ambiência e instalações de uma UPR.',
+    '5 - Dimensionamento de pessoal; segurança e saúde no trabalho.',
+    '6 - Gestão de resíduos em UPR. Ações sustentáveis em UPR.'
+  ]
+  },
+  {
+    faculdade: 'FCS',
+    area: 'Ciências dos alimentos',
+    pontosSorteados: [],
+    pontos: [
+    '1 - Estudo dos componentes alimentares.',
+    '2 - Classificação dos grupos alimentares.',
+    '3 - Toxicologia de alimentos.',
+    '4 - Boas práticas de fabricação de alimentos.',
+    '5 - Controle de qualidade de alimentos.'
+  ]
+  }]
