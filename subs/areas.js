15c2f28a146e7016c9504744bd3a93892cb7c2ab
ccsufgd <158065935+ccsufgd@users.noreply.github.com>
2024-07-18 12:26:58 -0400

    Update areas.js

--- subs/areas.js
+++ subs/areas.js
@@ -1,1 +1,474 @@
-a
+const areasData = [
+
+  {
+    faculdade: 'EAD',
+    area: 'Libras / Linguística da Libras',
+    pontosSorteados: [],
+    pontos: [
+			'1- Língua Brasileira de Sinais – Morfologia.',
+			'2- Língua Brasileira de Sinais – Sintaxe.',
+			'3- Ensino de Língua Brasileira de Sinais como primeira língua.',
+			'4- Ensino de Língua Brasileira de Sinais como segunda língua.',
+			'5- Introdução aos estudos linguísticos da Língua Brasileira de Sinais.'
+  			]
+  },
+  {
+    faculdade: 'EAD',
+    area: 'Letras / Língua Portuguesa',
+    pontosSorteados: [],
+    pontos: [
+			'1- Elementos de textualidade em Língua Portuguesa: coesão e coerência.',
+			'2- A concordância verbal na produção textual em Língua Portuguesa.',
+			'3- A gramática na sala de aula: das concepções às práticas de ensino de Língua Portuguesa.',
+			'4- Metodologia e estratégia para o ensino de Língua Portuguesa como segunda língua para surdos.',
+			'5- Tipologias e gêneros textuais: estratégias metodológicas para o ensino.'
+
+		 	]	
+  },
+  
+  	
+	{
+    faculdade: 'EAD',
+    area: 'Tradução e Interpretação em Libras / Língua Portuguesa',
+    pontosSorteados: [],
+    pontos: [
+		    '1- Interpretação comunitária e os papéis em diferentes espaços de atuação.',
+			'2- Concepção da tradução, papel e ética na prática do tradutor.',
+			'3- Estratégias de escolhas tradutórias no processo de elaboração de glosas.',
+			'4- Conceitos, tipologias e conscientização dos problemas teóricos e práticos da tradução.',
+			'5- Modalidades e Modelos de tradução e interpretação. '
+		 	]	
+  },
+	
+		
+	{
+    faculdade: 'EAD',
+    area: 'Educação Especial / Libras',
+    pontosSorteados: [],
+    pontos: [
+		    '1- Alunos público-alvo da educação especial e o atendimento das necessidades educacionais específicas de aprendizagem.',
+		    '2- Didática e educação de Surdos: currículo e metodologias de ensino.',
+		    '3- Fundamentos da Educação de Surdos: abordagens educacionais e legislação.',
+		    '4- Política linguística da Libras e modelos de gestão escolar inclusivo.',
+		    '5- O uso da TIC’s na educação de surdos.'
+		 	]	
+  },
+  
+	
+	{
+    faculdade: 'FACE',
+    area: 'Introdução à Economia',
+    pontosSorteados: [],
+    pontos: [
+		'1- As forças de mercado da oferta e da demanda.',
+		'2- Elasticidade e sua aplicação.',
+		'3- Os custos de produção.',
+		'4- Empresas em mercados competitivos.',
+		'5- Medindo a renda nacional.'
+
+		    
+		 	]	
+  },
+	
+	
+	
+	{
+    faculdade: 'FACE',
+    area: 'Contabilidade Avançada I e Contabilidade Avançada II',
+    pontosSorteados: [],
+    pontos: [
+		    '1- Método de Equivalência Patrimonial.',
+			'2- Combinação de negócios.',
+			'3- Instrumentos Financeiros.',
+			'4- Demonstrações consolidadas.',
+		    '5- Valor Justo considerando Ativos Biológicos.'
+
+		 	]	
+  },
+	
+	
+	{
+    faculdade: 'FACE',
+    area: 'Contabilidade Financeira / Análise das demonstrações contábeis',
+    pontosSorteados: [],
+    pontos: [
+		    '1- Análise de empresas por meio de indicadores econômicos e financeiros.',
+			'2- Análise de setores por meio de Índice padrão.',
+			'3- Método Dupont.',
+			'4- Administração de Capital de Giro.',
+			'5- Análise de Investimentos – VPL, TIR, Payback e análise de sensibilidade.'
+		 	]	
+  },
+	
+	{
+    faculdade: 'FACET',
+    area: 'Ciência da Computação / Algoritmos e Teoria da Computação I e II',
+    pontosSorteados: [],
+    pontos: [
+			'1- Vetores, matrizes e registros.',
+			'2- Algoritmos de Ordenação.',
+			'3- Autômatos Finitos.',
+			'4- Pilhas e Filas.',
+			'5- Linguagens Regulares.'
+		 	]	
+  },
+	{
+    faculdade: 'FACET',
+    area: 'Ensino de Química',
+    pontosSorteados: [],
+    pontos: [
+		    '1- Educação Ciência, Tecnologia, Sociedade (CTS) no ensino de química.',
+			'2- Estágio supervisionado: concepções, práticas de orientação, acompanhamento e avaliação.',
+			'3- Teorias de aprendizagem: aspectos cognitivos, sociais, culturais e afetivos envolvidos no Ensino de Química.',
+			'4- Pesquisa em ensino de química: caracterização e importância no Ensino de Química e na formação inicial e continuada de professores de química.',
+			'5- Possibilidades, desafios e perspectivas da experimentação no Ensino de Química. '
+		 	]	
+  },
+	{
+    faculdade: 'FACET',
+    area: 'Matemática / Estatística',
+    pontosSorteados: [],
+    pontos: [
+		    '1- Cálculo Diferencial: Análise do comportamento de funções e Problemas de Otimização.',
+			'2- Mudanças de Coordenadas: integrais duplas em coordenadas polares. integrais triplas: integrais triplas em coordenadas cilíndricas e esféricas.',
+			'3- Autovalores e Autovetores.',
+			'4- Medidas de Variabilidade: variância, desvio padrão e coeficiente de variação.',
+			'5- Distribuições de Probabilidade: binomial, Poisson e Normal.'
+		 	]	
+  },
+	{
+    faculdade: 'FACET',
+    area: 'Física / Física Geral',
+    pontosSorteados: [],
+    pontos: [
+			'1- Leis de conservação na mecânica newtoniana. ',
+			'2- Movimento oscilatório e suas aplicações. ',
+			'3- Fluídos. ',
+			'4- Leis da termodinâmica. ',
+			'5- Cargas Elétricas e Campos Elétricos. '
+		 	]	
+  },
+
+	
+	{
+    faculdade: 'FADIR',
+    area: 'Economia Política Internacional',
+    pontosSorteados: [],
+    pontos: [
+		    '1- Relações monetárias, financeiras, comerciais e de investimento em perspectiva histórica.',
+			'2- Reestruturação Produtiva e Globalização Econômica.',
+			'3- A OMC e a Governança do Sistema Multilateral de Comércio.',
+			'4- Arquitetura do Sistema Financeiro Internacional pós-crise de 2008.',
+			'5- Economia Política Internacional, Desenvolvimento e Dependência.'
+		 	]	
+  },
+	
+	{
+    faculdade: 'FADIR',
+    area: 'Direito Internacional',
+    pontosSorteados: [],
+    pontos: [	
+		    '1- A Personalidade Jurídica Internacional e os Sujeitos do Direito Internacional Público.',
+			'2- Conflitos entre Tratados Internacionais e Normas de Direito Interno no Brasil.',
+			'3- A Condição Jurídica dos Migrantes, Refugiados e Apátridas no Brasil.',
+			'4- Contratos Internacionais: Foro Competente e Legislação Aplicável.',
+			'5- Modalidades de Cooperação Jurídica Internacional.'
+		 	]	
+  },
+	
+	{
+    faculdade: 'FAED',
+    area: 'Pedagogia / Política e Gestão da Educação',
+    pontosSorteados: [],
+    pontos: [
+		    '1- Políticas educacionais para a Educação Básica.',
+			'2- Organização e gestão da educação escolar.',
+			'3- Gestão educacional e escolar.',
+			'4- Planejamento Educacional Brasileiro.',
+			'5- Medidas e avaliação da educação.'
+		 	]	
+  },
+	{
+    faculdade: 'FAED',
+    area: 'Pedagogia / Educação Especial',
+    pontosSorteados: [],
+    pontos: [
+		    '1- História da Educação Especial.',
+			'2- Políticas públicas de Educação Especial.',
+			'3- Inclusão escolar: raízes históricas, definição e características.',
+			'4- O atendimento educacional especializado voltado ao aluno público-alvo da Educação Especial.',
+			'5- Práticas pedagógicas inclusivas.'
+		 	]	
+  },
+	{
+    faculdade: 'FAED',
+    area: 'Educação Física Escolar',
+    pontosSorteados: [],
+    pontos: [
+		    '1- Corpo e movimento na escola: o trabalho da Educação Física no desenvolvimento infantil.',
+			'2- O estágio como campo de conhecimento: reflexões teórico-práticas.',
+			'3- Aspectos específicos do Estágio Supervisionado e da Prática de Ensino de Educação Física na Educação Infantil, no Ensino Fundamental e no Ensino Médio.',
+			'4- Educação no e para o lazer e suas relações com a Educação Física Escolar.',
+			'5- Gênero, Sexualidade e práticas corporais e esportivas.'
+		 	]	
+  },
+
+	
+	{
+    faculdade: 'FAEN',
+    area: 'Gerenciamento de Projetos Industriais',
+    pontosSorteados: [],
+    pontos: [
+		    '1- Planejamento e controle de projetos: escopo, cronograma e orçamento.',
+			'2- Teoria da decisão para estratégias de produção.',
+			'3- Gestão das variáveis de mercado e Marketing.',
+			'4- Previsão de Demanda: definição, características e métodos.',
+			'5- Projeto do arranjo físico industrial: centros de produção, fluxos e modelagem.'
+		 	]	
+  },
+	
+	{
+    faculdade: 'FAIND',
+    area: 'Educação',
+    pontosSorteados: [],
+    pontos: [
+			'1- Educação escolar indígena Guarani e Kaiowa no Ensino Fundamental.',
+			'2- Perspectivas teórico-práticas no Estágio Supervisionado no Ensino Fundamental na Educação Escolar Indígena.',
+			'3- Saberes Indígenas e a cultura escolar na Educação Escolar.',
+			'4- Metodologias de ensino para educação escolar nas escolas indígenas.',
+			'5- Processos de Letramento e numeramento nas escolas indígenas.'
+		 	]	
+  },
+	{
+    faculdade: 'FALE',
+    area: 'Literatura',
+    pontosSorteados: [],
+    pontos: [
+		    '1- Literatura Brasileira. ',
+			'2- Literatura Comparada. ',
+			'3- Literatura Portuguesa. ',
+			'4- O Ensino de Literatura nas Escolas Públicas. ',
+			'5- Teorias Literárias.'
+		 	]	
+  },
+	{
+    faculdade: 'FALE',
+    area: 'Linguística e Língua Portuguesa',
+    pontosSorteados: [],
+    pontos: [
+		    '1- Concepções teóricas de língua, linguagem, gramática e ensino de línguas. ',
+			'2- Gêneros discursivos acadêmicos.',
+			'3- Língua e discurso. ',
+			'4- Linguagem, sociedade e escola: norma, uso, variação e preconceito linguístico. ',
+			'5- Texto e textualidade no ensino de língua portuguesa e nos gêneros discursivos.'
+		 	]	
+  },
+	{
+    faculdade: 'FALE',
+    area: 'Processos de Criação e Pedagogia das Artes Cênicas',
+    pontosSorteados: [],
+    pontos: [
+		    '1- Abordagens metodológicas do ensino de atuação.',
+			'2- Treinamento físico: diretrizes para a composição do papel.',
+			'3- A atuação teatral e suas relações com outras áreas: circo, dança e performance.',
+			'4- Procedimentos metodológicos da pedagogia teatral contemporânea.',
+			'5- Linguagem Teatral e Educação Infantil.'
+		 	]	
+  },
+	{
+    faculdade: 'FALE',
+    area: 'Pedagogia das Artes Cênicas e Práticas Teatrais Contemporâneas ',
+    pontosSorteados: [],
+    pontos: [
+			'1- Possíveis articulações entre teorias e prática no Estágio Supervisionado e a formação artística pedagógica do docente/artista.',
+			'2- Artes circenses na educação básica, abordagens possíveis.',
+			'3- Perspectiva histórica de teatralidades não hegemônicas: indígena e/ou negra.',
+			'4- A pedagogia teatral contemporânea: procedimentos metodológicos e seus objetivos.',
+			'5- O ensino de teatro nos espaços formais e não formais da educação.'
+		 	]	
+  },
+	{
+    faculdade: 'FALE',
+    area: 'Metodologia do Ensino de Teatro',
+    pontosSorteados: [],
+    pontos: [
+		    '1- Teatro, educação e os movimentos de(s)coloniais.',
+			'2- Drama como método de ensino: perspectivas formais e não formais.',
+			'3- Os jogos teatrais e a criação de espetáculos no ambiente formal de ensino.',
+			'4- Teatro, política, movimentos sociais e opressão: processos artísticos e educacionais.',
+		    '5- Políticas públicas, teatro e a escola.'
+		 	]	
+  },
+	
+		{
+    faculdade: 'FCA',
+    area: 'Engenharia / Eletrônica e Eletrotécnica',
+    pontosSorteados: [],
+    pontos: [
+			'1- Luminotécnica e instalações elétricas residencial e predial.',
+			'2- Circuitos eletrônicos de acionamento e controle de potência DC.',
+			'3- Circuitos elétricos de acionamento e controle de potência AC.',
+			'4- Controle de fator de potência.',
+			'5- Tecnologias de automação para sistemas agrícolas e aquícolas.'
+		 	]	
+  },
+			{
+    faculdade: 'FCA',
+    area: 'Topografia e Geodésia',
+    pontosSorteados: [],
+    pontos: [
+			'1- Medida direta e indireta de distâncias.',
+			'2- Ângulos horizontais, ângulos verticais e ângulos de orientação.',
+			'3- Tipos de levantamento topográfico planimétrico.',
+			'4- Altimetria: Cotas e altitude. levantamento altimétrico.',
+			'5- Sistemas sensores: Caracterização, classificação. níveis de coleta de dados espectral.'	    
+		 	]	
+  },
+				{
+    faculdade: 'FCBA',
+    area: 'Ensino de Ciências',
+    pontosSorteados: [],
+    pontos: [
+			'1- Teorias de aprendizagem.',
+			'2- Planejamento e Avaliação da aprendizagem no Ensino de Ciências e Biologia.',
+			'3- Estágio e docência na formação do professor de Ciências e Biologia.',
+			'4- Tendências e Perspectivas do Ensino de Ciências e Biologia.',
+			'5- Tecnologia no ensino de ciências e biologia.'
+		 	]	
+  },
+				{
+    faculdade: 'FCH',
+    area: 'Ciências Sociais / Antropologia',
+    pontosSorteados: [],
+    pontos: [
+		    '1- Teorias Antropológicas.',
+			'2- Antropologia no Brasil.',
+			'3- A questão da inserção do negro na sociedade nacional: a mestiçagem.',
+			'4- Cultura, identidade, relações interétnicas e políticas da alteridade.',
+			'5- Abordagens teórico-metodológicas da pesquisa de campo em antropologia.'
+		 	]	
+  },
+				{
+    faculdade: 'FCH',
+    area: 'Ciências Sociais / Sociologia',
+    pontosSorteados: [],
+    pontos: [
+		    '1- Sociologia e modernidade: o surgimento da "ciência da sociedade".',
+			'2- Introdução à Sociologia Clássica: Marx, Durkheim e Weber.',
+			'3- Sociologia Contemporânea.',
+			'4- Pensamento Social no Brasil.',
+			'5- Educação em Direitos Humanos.'
+		 	]	
+  },
+				{
+    faculdade: 'FCH',
+    area: 'Ensino de Geografia',
+    pontosSorteados: [],
+    pontos: [
+		    '1- O Estágio Supervisionado na formação do professor de Geografia. ',
+			'2- Propostas Curriculares para o ensino de Geografia. ',
+			'3- Educação, escola e ensino de Geografia na sociedade contemporânea. ',
+			'4- Cartografia escolar e ensino de Geografia. ',
+			'5- Linguagens e ensino de Geografia. '
+		 	]	
+  },
+				{
+    faculdade: 'FCH',
+    area: 'Psicoterapia Cognitiva',
+    pontosSorteados: [],
+    pontos: [
+		    '1- História e fundamentos filosóficos da Psicologia Cognitiva.',
+			'2- Neurociência Cognitiva.',
+			'3- Conceitualização Cognitiva de Casos Clínicos.',
+			'4- Técnicas da Terapia Cognitiva-Comportamental.',
+			'5- Terapia Cognitiva e Psicopatologia.'
+		 	]	
+  },
+				{
+    faculdade: 'FCH',
+    area: 'Psicologia Escolar',
+    pontosSorteados: [],
+    pontos: [
+			'1- História do ensino e da escola. Psicologia Escolar.',
+			'2- Psicologia Escolar e Inclusão Escolar.',
+			'3- Queixa escolar, aprendizagem e medicalização.',
+			'4- Psicologia Escolar e Educação Inclusiva.',
+			'5- A atuação do psicólogo na escola. legislação e normas.'
+		 	]	
+  },
+				{
+    faculdade: 'FCH',
+    area: 'Desenvolvimento, aprendizagem e formação de professoras(es)',
+    pontosSorteados: [],
+    pontos: [
+			'1- Desenvolvimento e Aprendizagem: diferentes perspectivas teóricas.',
+			'2- Avaliação da aprendizagem: pressupostos teóricos e metodológicos da avaliação em diferentes momentos históricos.',
+			'3- Funções, finalidades e modalidades da avaliação da aprendizagem.',
+			'4- Psicologia. Formação docente. Licenciatura em Psicologia.',
+			'5- Instrumentos e critérios na avaliação da aprendizagem no contexto escolar.'
+		 	]	
+  },
+				{
+    faculdade: 'FCH',
+    area: 'Psicologia Fenomenológico-Existencial',
+    pontosSorteados: [],
+    pontos: [
+			'1- O problema da consciência na fenomenologia.',
+			'2- Sentido ético-político e antropológico da teoria do Self em Gestalt-Terapia.',
+			'3- Modelos de Psicologia fenomenológico-existenciais e humanistas.',
+			'4- Fundamentos fenomenológico-existenciais da prática clínica.',
+			'5- Contribuições da Fenomenologia para o aconselhamento psicológico.'
+		 	]	
+  },
+				{
+    faculdade: 'FCS',
+    area: 'Ciências morfofuncionais',
+    pontosSorteados: [],
+    pontos: [
+			'1- Inflamação aguda.',
+			'2- Apoptose.',
+			'3.Cardiopatia Valvular.',
+			'4- Mecanismo das reações de hipersensibilidade.',
+			'5- Anemia por diminuição da eritropoese.'
+		 	]	
+  },
+				{
+    faculdade: 'FCS',
+    area: 'Clínica Médica',
+    pontosSorteados: [],
+    pontos: [
+			'1- Lúpus eritematoso sistêmico.',
+			'2- Sarcoidose.',
+			'3- Doenças pulmonares intersticiais difusas crônicas.',
+			'4- Febre reumática aguda.',
+			'5- Tumores ósseos.'
+		 	]	
+  },
+								{
+    faculdade: 'FCS',
+    area: 'Semiologia médica',
+    pontosSorteados: [],
+    pontos: [
+		    '1- Anamnese.',
+			'2- Dor.',
+			'3- Exame físico do sistema cardiovascular.',
+			'4- Dispneia.',
+			'5- Doenças da parede e da cavidade abdominal.'
+		 	]	
+  },
+												{
+    faculdade: 'FCS',
+    area: 'Ginecologia e Obstetrícia',
+    pontosSorteados: [],
+    pontos: [
+		    '1- Atenção e Assistência ao Pré-natal. ',
+			'2- Avaliação da vitalidade fetal. ',
+			'3- Assistência ao parto. ',
+			'4- Contracepção. ',
+			'5- Avaliação no sangramento uterino anormal. '
+		 	]	
+  }
+
+]
+
+	
