52c86555e2b835d35b7eca1b21898a87bb61dc3f
ccsufgd <158065935+ccsufgd@users.noreply.github.com>
2024-11-13 11:02:29 -0400

    Update scriptbkp.js

--- subs/2025-1/scriptbkp.js
+++ subs/2025-1/scriptbkp.js
@@ -4,9 +4,40 @@ console.log(areas);
 
 const resultadosSorteio = [];
 
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowRight' || event.key === 'ArrowLeft') {
+        scrollToSection(event.key);
+    }
+});
 
+function scrollToSection(keyPressed) {
+    const sections = document.querySelectorAll('section'); // Seleciona todas as seções da página
 
+    let currentSectionIndex = -1;
 
+    // Encontra o índice da seção atualmente visível
+    sections.forEach((section, index) => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+            currentSectionIndex = index;
+        }
+    });
+
+    let targetIndex;
+
+    // Determina o índice da seção de destino com base na tecla pressionada
+    if (keyPressed === 'ArrowRight') {
+        targetIndex = currentSectionIndex + 1;
+    } else if (keyPressed === 'ArrowLeft') {
+        targetIndex = currentSectionIndex - 1;
+    }
+
+    // Verifica se a seção de destino é válida e rola para ela
+    if (targetIndex >= 0 && targetIndex < sections.length) {
+        const targetSection = sections[targetIndex];
+        targetSection.scrollIntoView({ behavior: 'smooth', block: 'center' }); // Rola para a seção de destino e centraliza verticalmente
+    }
+}
 console.log(areas.length);
 let areasContador = areas.length;
 let contadorElement = document.getElementById('contador');
@@ -14,14 +45,16 @@ contadorElement.innerHTML = `Áreas: ${areasContador}</strong>`;
 
 
 
+
 function imprimirArea(area) {
   const container = document.getElementById('areasContainer');
 
   // Criar elementos HTML
-  const areaElement = document.createElement('div');
-  areaElement.innerHTML = `<hr></hr>
+  const areaElement = document.createElement('section');
+  areaElement.innerHTML = `<div><hr></hr>
     <p class="area-title"><strong style="font-size: 1.3em;">${area.area}</strong></p>
-    <p class="area-title">${area.faculdade}</p>`;
+    <p class="area-title">${area.faculdade}</p>
+    </div>`;
 
   const tabelaElement = document.createElement('table');
   tabelaElement.classList.add('table', 'table-striped', 'table-bordered'); // Adiciona as classes do Bootstrap
@@ -107,10 +140,10 @@ function realizarSorteio() {
 
     resultadosSorteio.push(resultado);
 
-    const resultadoElement = document.createElement('div');
+    const resultadoElement = document.createElement('section');
     resultadoElement.className = 'resultado-area';
 
-    resultadoElement.innerHTML = `
+    resultadoElement.innerHTML = `<div>
       <hr>
       <br>
       <p class="area-title"><strong style="font-size: 1.3em;">${resultado.area} - ${resultado.faculdade}</strong></p>
@@ -121,7 +154,7 @@ function realizarSorteio() {
       <br>
       <br>
 
-
+     </div>
     `;
 
     // Append the resultadoElement to the DOM or wherever you want to display it
@@ -151,64 +184,79 @@ document.getElementById("resultadoDiv").style.display = "block";
 }
 
 function sortearPonto(pontos, area) {
-  const infoContainer = document.getElementById('infoContainer');
+    const infoContainer = document.getElementById('infoContainer');
+  
+    // Shuffle the points array
+    const pontosOrd = pontos
+    const pontosEmbaralhados = shuffleArray(pontos);
 
-  // Adicionar texto apenas se ainda não foi adicionado
-  if (!print) {
-    infoContainer.innerHTML += '<br><hr>';
-    infoContainer.innerHTML += '<div class="alert alert-success" role="alert"><p class="area-title"><strong>Arquivo de Logs</strong></p></div>';
-    infoContainer.innerHTML += '<br>';
-    print = true;
-  }
-
-  // logs de identificação
-  console.log(`Sorteio para a área: ${area.area} (Faculdade: ${area.faculdade})`);
-  console.log('Os pontos disponíveis para sorteio da Prova Didática', pontos);
-  infoContainer.innerHTML += `
-    <div class="alert alert-dark" role="alert" style="text-align: center;">
-    <br>
-      <p class="area-title">
-      <strong>${area.area}</strong> (Faculdade: ${area.faculdade})</p>
+    // Adicionar texto apenas se ainda não foi adicionado
+    if (!print) {
+      infoContainer.innerHTML += '<br><hr>';
+      infoContainer.innerHTML += '<div class="alert alert-success" role="alert"><p class="area-title"><strong>Arquivo de Logs</strong></p></div>';
+      infoContainer.innerHTML += '<br>';
+      print = true;
+    }
+  
+    // logs de identificação
+    console.log(`Sorteio para a área: ${area.area} (Faculdade: ${area.faculdade})`);
+    console.log('Os pontos disponíveis para sorteio da Prova Didática:', pontosOrd);
+    console.log('Os pontos embaralhados:', pontosEmbaralhados);
+    infoContainer.innerHTML += `
+      <div class="alert alert-dark" role="alert" style="text-align: center;">
       <br>
-    </div>`;
-  infoContainer.innerHTML += `<p><strong>Os pontos disponíveis para sorteio da Prova Didática:</strong> <br>${pontos.join('<br> ')}</p>`;
-
-  const randomBytes = new Uint8Array(1);
-  crypto.getRandomValues(randomBytes);
-  crypto.getRandomValues(randomBytes);
-  const indiceAleatorio = randomBytes[0] % pontos.length;
-  const pontoSorteado = pontos[indiceAleatorio];
-
-  // logs de identificação
-  console.log('Ponto sorteado Prova Didática:', pontoSorteado);
-  console.log('Finalizado');
-
-  infoContainer.innerHTML += `
-    <div class="alert alert-success" role="alert" style="text-align: center;">
-    <p class="area-title">Ponto sorteado para <strong>Prova Didática:</strong><br>
-    <strong>${pontoSorteado}</strong>
-    </div>`;
-
-  infoContainer.innerHTML += `
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col"><strong>Resultado Sorteio - ${area.area}</strong> (Faculdade: ${area.faculdade})</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td><strong>Prova Didática:</strong> ${pontoSorteado}</td>
-          </tr>
-        </tbody>
-      </table>
-    `;
-
-  infoContainer.innerHTML += `<div class="alert alert-info" role="alert">
-   <p class="area-title">Finalizado!</p></div><br><hr></hr>`;
-
-  return pontoSorteado;
-}
+        <p class="area-title">
+        <strong>${area.area}</strong> (Faculdade: ${area.faculdade})</p>
+        <br>
+      </div>`;
+    infoContainer.innerHTML += `<p><strong>Os pontos disponíveis para sorteio da Prova Didática:</strong> <br>${pontosOrd.join('<br> ')}</p>`;
+  
+    const randomBytes = new Uint8Array(1);
+    crypto.getRandomValues(randomBytes);
+    const indiceAleatorio = randomBytes[0] % pontosEmbaralhados.length;
+    const pontoSorteado = pontosEmbaralhados[indiceAleatorio];
+  
+    // logs de identificação
+    console.log('Ponto sorteado Prova Didática:', pontoSorteado);
+    console.log('Finalizado');
+  
+    infoContainer.innerHTML += `
+      <div class="alert alert-success" role="alert" style="text-align: center;">
+      <p class="area-title">Ponto sorteado para <strong>Prova Didática:</strong><br>
+      <strong>${pontoSorteado}</strong>
+      </div>`;
+  
+    infoContainer.innerHTML += `
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col"><strong>Resultado Sorteio - ${area.area}</strong> (Faculdade: ${area.faculdade})</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><strong>Prova Didática:</strong> ${pontoSorteado}</td>
+            </tr>
+          </tbody>
+        </table>
+      `;
+  
+    infoContainer.innerHTML += `<div class="alert alert-info" role="alert">
+     <p class="area-title">Finalizado!</p></div><br><hr></hr>`;
+  
+    return pontoSorteado;
+  }
+  
+  // Function to shuffle array
+  function shuffleArray(array) {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+    }
+    return shuffledArray;
+  }
+  
 
 
 
@@ -258,9 +306,10 @@ function viewResults() {
   sorteioContainer.innerHTML = ''; // Clear previous results
 
   resultadosSorteio.forEach(resultado => {
-    const resultadoElement = document.createElement('div');
+    const resultadoElement = document.createElement('section');
     resultadoElement.className = 'resultado-area';
     resultadoElement.innerHTML = `
+    <div>
       <hr>
       <br>
       <p class="area-title"><strong style="font-size: 1.3em;">${resultado.area} - ${resultado.faculdade}</strong></p>
@@ -271,7 +320,7 @@ function viewResults() {
       <br>
       <br>
 
-
+    </div>
     `;
     sorteioContainer.appendChild(resultadoElement);
   });
@@ -361,6 +410,7 @@ function mostrarLog() {
 
   infoContainer.style.display = 'block';
   window.scrollTo({ top: 0, behavior: 'smooth' });
+  window.print()
 }
 
 
@@ -393,5 +443,3 @@ function exibirAlerta(tipo, mensagem) {
 function ocultarAlerta() {
   const customAlert = document.getElementById('customAlert');
   customAlert.style.display = 'none';
-}
-
