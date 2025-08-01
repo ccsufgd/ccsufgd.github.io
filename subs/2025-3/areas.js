518445907c23241647f165f7e87817c08cdcaa97
ccsufgd <158065935+ccsufgd@users.noreply.github.com>
2025-08-01 10:05:56 -0400

    Update areas.js

--- subs/2025-3/areas.js
+++ subs/2025-3/areas.js
@@ -1,184 +1,158 @@
 const areasData = [
-{
-        "faculdade": "FCS",
-        "area": "Dietética nos Ciclos da Vida e Nutrição Esportiva",
-        "pontosSorteados": [],
-        "pontos": [
-            "1. Classificação NOVA e o Guia alimentar para a população brasileira;",
-            "2. Avaliação do estado nutricional de praticantes de atividade física;",
-            "3. Planejamento de intervenção nutricional para diferentes modalidades esportivas, em diferentes momentos da competição;",
-            "4. Fisiologia da gestação;",
-            "5. Avaliação do estado nutricional e planejamento dietético para gestantes."
-        ]
-    },
-    {
-        "faculdade": "FADIR",
-        "area": "Direito/Prática Jurídica",
-        "pontosSorteados": [],
-        "pontos": [
-            "1. Conciliação, mediação e arbitragem.",
-            "2. Do procedimento comum: aspectos essenciais da petição inicial e da contestação.",
-            "3. Direitos do consumidor: características e princípios norteadores da relação de consumo.",
-            "4. Teoria Geral dos recursos e recursos cíveis em espécies.",
-            "5. Alimentos: conceito, características, espécies, aspectos materiais e processuais."
-        ]
-    },
-        {
-        "faculdade": "FAED",
-        "area": "Educação Física Escolar",
-        "pontosSorteados": [],
-        "pontos": [
-            "1. Corpo, movimento e expressão corporal na escola: o trabalho da Educação Física no desenvolvimento infantil.",
-            "2. O estágio como campo de conhecimento: reflexões teórico-práticas.",
-            "3. Teoria e prática da dança na Educação Física Escolar.",
-            "4. Teoria e prática da ginástica na Educação Física Escolar.",
-            "5. Gênero, Sexualidade e práticas corporais e esportivas."
-        ]
-    },
-    {
-        "faculdade": "FAEN",
-        "area": "Engenharia de Produção/Gerenciamento Ambiental",
-        "pontosSorteados": [],
-        "pontos": [
-            "1. Metodologia Científica para Engenharia de Produção: Importância da metodologia científica na pesquisa e desenvolvimento de projetos em engenharia.",
-            "2. Gerenciamento de Recursos Naturais e Energéticos: Estratégias para a utilização eficiente de recursos energéticos renováveis e não renováveis;",
-            "3. Segurança e Saúde do Trabalho: Normas e regulamentações de segurança no ambiente de trabalho;",
-            "4. Gerenciamento Ambiental: Planejamento e implementação de sistemas de gestão ambiental;",
-            "5. ESG (Environmental, Social, and Governance): Implementação de práticas ESG para melhorar a sustentabilidade e responsabilidade social."
-        ]
-    },
-{
-        "faculdade": "FCH",
-        "area": "Estágio supervisionado em História/metodologias do ensino e da pesquisa em História",
-        "pontosSorteados": [],
-        "pontos": [
-            "1. O estágio supervisionado na formação do professor de História;",
-            "2. Currículo e ensino de História;",
-            "3. Metodologias da História e interdisciplinaridade;",
-            "4. Fontes para o ensino de História;",
-            "5. O professor e a pesquisa histórica."
-        ]
-    },
- {
-        "faculdade": "FAEN",
-        "area": "Estruturas/Solos/Instalações Prediais",
-        "pontosSorteados": [],
-        "pontos": [
-            "1. Mecânica vetorial de forças e Momentos.",
-            "2. Análise estrutural (treliças, método dos nós e das seções).",
-            "3. Propriedades mecânicas dos materiais.",
-            "4. Atribuições, direitos e deveres do profissional da Engenharia Civil.",
-            "5. Instalações prediais de água fria."
-        ]
-    },
- {
-        "faculdade": "FCH",
-        "area": "Geografia Humana",
-        "pontosSorteados": [],
-        "pontos": [
-            "1. Lugar e identidade na Geografia;",
-            "2. Gênese da Geografia Cultural;",
-            "3. Formação socioespacial brasileira;",
-            "4. Relações entre cultura, sociedade e espaço;",
-            "5. Metodologias qualitativas em trabalho de campo."
-        ]
-    },
-{
-        "faculdade": "FALE",
-        "area": "Linguística e Língua Portuguesa",
-        "pontosSorteados": [],
-        "pontos": [
-            "1. Concepções teóricas de língua, linguagem e gramática e o ensino de línguas;",
-            "2. Gêneros discursivos acadêmicos;",
-            "3. Língua e discurso;",
-            "4. Linguagem, sociedade e escola: norma, uso, variação e preconceito linguístico;",
-            "5. Texto e textualidade no ensino de língua portuguesa e nos gêneros discursivos."
-        ]
-    },
-{
-        "faculdade": "FAED",
-        "area": "Pedagogia/Educação Especial",
-        "pontosSorteados": [],
-        "pontos": [
-            "1. História da Educação Especial.",
-            "2. Políticas públicas de Educação Especial.",
-            "3. Inclusão escolar: raízes históricas, definição e características.",
-            "4. O atendimento educacional especializado voltado ao aluno público-alvo da Educação Especial.",
-            "5. Práticas pedagógicas inclusivas."
-        ]
-    },
-{
-        "faculdade": "FAED",
-        "area": "Pedagogia/História da Educação",
-        "pontosSorteados": [],
-        "pontos": [
-            "1. História e historiografia da Educação Brasileira.",
-            "2. Aspectos teórico-metodológicos da pesquisa em História da Educação.",
-            "3. Perspectivas de pesquisa em Memória e História da Educação.",
-            "4. História da Educação e instituições educativas.",
-            "5. História da Educação, práticas e representações."
-        ]
-    },
-{
-        "faculdade": "FAED",
-        "area": "Pedagogia/Política e Gestão da Educação",
-        "pontosSorteados": [],
-        "pontos": [
-            "1. Políticas educacionais para a Educação Básica.",
-            "2. Organização e gestão da educação escolar.",
-            "3. Gestão educacional e escolar.",
-            "4. Planejamento Educacional Brasileiro.",
-            "5. Medidas e avaliação da educação."
-        ]
-    },
-        {
-        "faculdade": "FCH",
-        "area": "Psicologia / Orientação Profissional",
-        "pontosSorteados": [],
-        "pontos": [
-            "1. Teorias da motivação no trabalho;",
-            "2. Percepção organizacional;",
-            "3. Saúde mental no trabalho;",
-            "4. Análise crítica em Orientação Profissional e de Carreira;",
-            "5. Orientação Profissional e de Carreira na interface com o mundo do trabalho."
-        ]
-    },
-{
-        "faculdade": "FCH",
-        "area": "Psicologia/Psicologia do Desenvolvimento",
-        "pontosSorteados": [],
-        "pontos": [
-            "1. Diferentes perspectivas em desenvolvimento humano;",
-            "2. Estabilidade e mudança durante o ciclo vital;",
-            "3. Pesquisa e intervenção no campo de desenvolvimento humano: limites e possibilidades;",
-            "4. Teorias da Personalidade. Análise da estrutura, dinâmica e desenvolvimento da personalidade segundo diferentes teorias. Influências do temperamento no desenvolvimento humano;",
-            "5. Intervenção precoce e múltiplos olhares sobre as potencialidades e necessidades das famílias."
-        ]
-    },
- {
-        "faculdade": "FADIR",
-        "area": "Relações Internacionais/Política Internacional",
-        "pontosSorteados": [],
-        "pontos": [
-            "1. Organismos multilaterais e a governança global.",
-            "2. Cooperação Internacional e Governança Global.",
-            "3. Meio ambiente, crise climática e política global.",
-            "4. Integração Regional e fluxos transfronteiriços.",
-            "5. Direitos Humanos, Soberania e Relações Internacionais."
-        ]
-    },
- {
-        "faculdade": "FAEAD",
-        "area": "Tradução e Interpretação em Libras/Língua Portuguesa",
-        "pontosSorteados": [],
-        "pontos": [
-            "1. Interpretação comunitária e os papéis em diferentes espaços de atuação.",
-            "2. Estratégias de escolhas tradutórias no processo de elaboração de glosas.",
-            "3. Conceitos, tipologias e conscientização dos problemas teóricos e práticos da tradução.",
-            "4. Metodologia do Ensino de Libras como L2.",
-            "5. Metodologia do Ensino de Libras como L1."
-        ]
-    }
-    
+  {
+    "faculdade": "FCS",
+    "area": "Anatomia Cirúrgica",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Anatomia do fígado e vias biliares e abordagens cirúrgicas: hepatectomia e colecisctectomia",
+      "2- Parede abdominal",
+      "3- Cavidade abdominal e espaços retroperitoneais",
+      "4- Anatomia da pelve: superficial, ossos, músculos, vísceras",
+      "5- Anatomia das pleuras e pulmões"
+    ]
+  },
+  {
+    "faculdade": "FACET",
+    "area": "Ciência da Computação /Algoritmos e Inteligência Artificial",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Notação Assintótica",
+      "2- Método do Guloso",
+      "3- Programação dinâmica",
+      "4- Aprendizagem de máquina",
+      "5- Mineração de Dados"
+    ]
+  },
+  {
+    "faculdade": "FAIND",
+    "area": "Ciências da Natureza",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Biodiversidade, ecossistemas, interações entre os seres vivos no ensino e na formação intercultural de professores de Ciências/Biologia",
+      "2- Interculturalidade e TICs no ensino e na formação de professores de Ciências",
+      "3- Educação para a Saúde e Educação Ambiental na escola indígena intercultural;",
+      "4- Saberes tradicionais e ciência ocidental no currículo de formação de professores indígenas: perspectivas teóricas e metodológicas",
+      "5- Alterações da paisagem e mudanças climáticas: desafios para a gestão ambiental e territorial nas aldeias Guarani e Kaiowá em MS"
+    ]
+  },
+  {
+    "faculdade": "FCS",
+    "area": "Clínica Médica: Infectologia",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Tuberculose.",
+      "2- Endocardite infecciosa.",
+      "3- Infecções respiratórias virais comuns.",
+      "4- Infecções nasocomiais.",
+      "5- Resistência bacterianas a agentes antimicrobianos."
+    ]
+  },
+  {
+    "faculdade": "FACE",
+    "area": "Contabilidade para usuário externo/Contabilidade Financeira",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Apuração, registro e controle do LALUR",
+      "2- Lucro Real X Lucro Presumido",
+      "3- Auditoria interna e externa",
+      "4- Combinação de Negócios",
+      "5- Demonstração dos Fluxos de Caixa"
+    ]
+  },
+  {
+    "faculdade": "FADIR",
+    "area": "Direito / Prática Jurídica",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Conciliação, mediação e arbitragem;",
+      "2- Do procedimento comum: aspectos essenciais da petição inicial e da contestação;",
+      "3- Direitos do consumidor: características e princípios norteadores da relação de consumo;",
+      "4- Teoria Geral dos recursos e recursos cíveis em espécies;",
+      "5- Alimentos: conceito, características, espécies, aspectos materiais e processuais."
+    ]
+  },
+  {
+    "faculdade": "FAED",
+    "area": "Estudos Linguísticos e Estudos Linguísticos da Libras",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Introdução aos estudos linguísticos da Língua Brasileira de Sinais;",
+      "2- Estudos Linguísticos da Língua Brasileira de Sinais – Morfologia;",
+      "3- Estudos Linguísticos da Língua Brasileira de Sinais – Sintaxe;",
+      "4- Ensino de Língua Brasileira de Sinais como primeira língua;",
+      "5- Ensino de Língua Brasileira de Sinais como segunda língua."
+    ]
+  },
+  {
+    "faculdade": "FACET",
+    "area": "Física/Ensino de Física",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Experimentação no ensino de Física;",
+      "2- Teorias da aprendizagem para o ensino de Física;",
+      "3- Divulgação científica e o ensino de Física;",
+      "4- Formação inicial e continuada de professores de Física;",
+      "5- História da ciência, epistemologia e ensino de Física."
+    ]
+  },
+  {
+    "faculdade": "FCH",
+    "area": "Geografia Física",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Climas do Brasil: massas de ar, frentes e zonas de convergências",
+      "2- Princípios e conceitos de ecologia aplicados à Biogeografia, a distribuição geográfica dos seres vivos",
+      "3- A bacia hidrográfica como unidade territorial para a gestão de recursos hídricos",
+      "4- Solos arenosos no Brasil: formação, compartimentação morfopedológica, potencialidades e fragilidades ambientais",
+      "5- Sistemas de Informações Geo-Referenciadas: conceitos e aplicações em estudos ambientais"
+    ]
+  },
+  {
+    "faculdade": "FCH",
+    "area": "Geografia Humana",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- O direito à cidade: lutas e resistências",
+      "2- Modo de produção capitalista e questão agrária no Brasil",
+      "3- Formação socioespacial brasileira",
+      "4- Relações entre cultura, sociedade e espaço",
+      "5- Estado, fronteiras, culturas e identidades"
+    ]
+  },
+  {
+    "faculdade": "FCH",
+    "area": "Psicopatologia e Análise Institucional",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- História da Psicopatologia no Brasil",
+      "2- Psicopatologia e Neurodiversidade",
+      "3- Estruturas Clínicas",
+      "4- Neoliberalismo e adoecimento Psíquico",
+      "5- O sujeito dobradiça"
+    ]
+  },
+  {
+    "faculdade": "FCS",
+    "area": "Saúde Pública",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Política Nacional de Alimentação e Nutrição (PNAN)",
+      "2- Políticas e Programas intersetoriais de Alimentação e Nutrição: histórico e avanços",
+      "3- Vigilância Alimentar e Nutricional",
+      "4- Saúde e Nutrição de Povos Indígenas do Brasil",
+      "5- Políticas Públicas de Saúde Indígena"
+    ]
+  },
+  {
+    "faculdade": "FALE",
+    "area": "Voz e Cena",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Voz falada e voz cantada: aquecimento e afinação.",
+      "2- Visitações interculturais em referências vocais contemporâneas no teatro.",
+      "3- Preparações vocais e reverberações sonoras: teatro de sala e teatro de rua.",
+      "4- Voz e discurso nos processos criativos teatrais.",
+      "5- Possibilidades vocais múltiplas para atrizes e atores: do teatro musical à dublagem."
+    ]
+  }
 ]
-    
