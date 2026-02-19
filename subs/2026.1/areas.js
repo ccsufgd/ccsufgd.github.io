774606d52061f1bc46a2bb97b8891af19519287e
ccsufgd <158065935+ccsufgd@users.noreply.github.com>
2026-02-19 09:17:49 -0400

    Create areas.js

---
+++ subs/2026.1/areas.js
@@ -0,0 +1,338 @@
+const areasData = [
+  {
+    "faculdade": "FACE",
+    "area": "Contabilidade para usuário externo/Contabilidade Financeira",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Demonstração do Resultado do Exercício (versão 2026)",
+      "2- Auditoria Externa",
+      "3- Auditoria Interna",
+      "4- Combinação de Negócios",
+      "5- Demonstração dos Fluxos de Caixa"
+    ]
+  },
+  {
+    "faculdade": "FACET",
+    "area": "Física/Ensino de Física",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Experimentação no ensino de Física",
+      "2- Teorias da aprendizagem para o ensino de Física",
+      "3- Divulgação científica e o ensino de Física",
+      "4- Formação inicial e continuada de professores de Física",
+      "5- História da ciência, epistemologia e ensino de Física"
+    ]
+  },
+  {
+    "faculdade": "FACET",
+    "area": "Física Geral",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Mecânica newtoniana",
+      "2- Oscilações e Ondas",
+      "3- Fluidos",
+      "4- Leis da Termodinâmica",
+      "5- Equações de Maxwell"
+    ]
+  },
+  {
+    "faculdade": "FACET",
+    "area": "Ensino de Química",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Educação Ciência, Tecnologia, Sociedade (CTS) no ensino de química",
+      "2- Estágio supervisionado: concepções, práticas de orientação, acompanhamento e avaliação",
+      "3- Teorias de aprendizagem: aspectos cognitivos, sociais, culturais e afetivos envolvidos no Ensino de Química",
+      "4- Pesquisa em ensino de química: caracterização e importância no Ensino de Química e na formação inicial e continuada de professores de química",
+      "5- Possibilidades, desafios e perspectivas da experimentação no Ensino de Química"
+    ]
+  },
+  {
+    "faculdade": "FAEAD",
+    "area": "Educação Especial: Libras e Deficiência Auditiva",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Introdução aos estudos linguísticos da Língua Brasileira de Sinais",
+      "2- Fundamentos da Educação de Surdos",
+      "3- Libras, Politica e Gestão",
+      "4- Ensino de Língua Brasileira de Sinais como primeira língua",
+      "5- Ensino de Língua Brasileira de Sinais como segunda língua"
+    ]
+  },
+  {
+    "faculdade": "FAED",
+    "area": "Praticas Corporais e Esportivas",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- A cultura corporal do movimento na Educação Física escolar",
+      "2- Fundamentos teórico-metodológicos do ensino das lutas e/ou esportes de combate na Educação Física Escolar",
+      "3- Jogos de oposição no ensino das lutas e/ou esportes de combate na Educação Física Escolar",
+      "4- Abordagens metodológicas para o ensino da dança: perspectivas históricas, críticas e contemporâneas",
+      "5- A presença da dança no currículo da Educação Física escolar: sensibilidade, inclusão, socialização e expressão cultural"
+    ]
+  },
+  {
+    "faculdade": "FAED",
+    "area": "Formação de Professores(as), Currículo e Estágio Supervisionado",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Didática e formação de professores",
+      "2- Teoria e prática do currículo",
+      "3- Avaliação da aprendizagem",
+      "4- Desafios do estágio supervisionado na Pedagogia",
+      "5- Planejamento das práticas pedagógicas"
+    ]
+  },
+  {
+    "faculdade": "FAED",
+    "area": "Pedagogia / Política e Gestão da Educação",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Políticas educacionais para a Educação Básica",
+      "2- Organização e gestão da educação escolar",
+      "3- Gestão educacional e escolar",
+      "4- Planejamento Educacional Brasileiro",
+      "5- Medidas e avaliação da educação"
+    ]
+  },
+  {
+    "faculdade": "FAED",
+    "area": "Pedagogia / Educação Especial",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- História da Educação Especial",
+      "2- Políticas públicas de Educação Especial",
+      "3- Inclusão escolar: raízes históricas, definição e características",
+      "4- O atendimento educacional especializado voltado ao aluno público-alvo da Educação Especial",
+      "5- Práticas pedagógicas inclusivas"
+    ]
+  },
+  {
+    "faculdade": "FAIND",
+    "area": "Ciências da Natureza",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Biodiversidade, ecossistemas, interações entre os seres vivos no ensino e na formação intercultural de professores de Ciências/Biologia",
+      "2- Interculturalidade e TICs no ensino e na formação de professores de Ciências",
+      "3- Educação para a Saúde e Educação Ambiental na escola indígena intercultural",
+      "4- Saberes tradicionais e ciência ocidental no currículo de formação de professores indígenas: perspectivas teóricas e metodológicas",
+      "5- Alterações da paisagem e mudanças climáticas: desafios para a gestão ambiental e territorial nas aldeias Guarani e Kaiowá em MS"
+    ]
+  },
+  {
+    "faculdade": "FAIND",
+    "area": "Ciências Humanas",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Estudos Interdisciplinares sobre a América Latina",
+      "2- Estudos Interdisciplinares sobre o Brasil",
+      "3- Ciências Humanas e interdisciplinaridade na educação do campo",
+      "4- História, memória e ensino aplicados à educação do campo",
+      "5- Epistemologia das ciências humanas aplicada à educação do campo"
+    ]
+  },
+  {
+    "faculdade": "FAIND",
+    "area": "Educação",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Educação escolar indígena Guarani e Kaiowa nos anos iniciais do Ensino Fundamental",
+      "2- Perspectivas teórico-práticas no Estágio Supervisionado na Educação Infantil e Anos Iniciais do Ensino Fundamental nas Escola Indígena",
+      "3- Saberes Indígenas e a cultura escolar na Educação Escolar",
+      "4- Metodologias de ensino para educação infantil e anos iniciais nas escolas indígenas",
+      "5- Processos de alfabetização, letramento e numeramento na língua guarani, para os anos iniciais das escolas indígenas"
+    ]
+  },
+  {
+    "faculdade": "FALE",
+    "area": "Voz, Música e Cena",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Voz falada e voz cantada: aquecimento e afinação",
+      "2- Visitações interculturais em referências vocais e musicais contemporâneas no teatro",
+      "3- Educação musical para atrizes e atores: práticas que podem visitar exercícios inclusivos ou a musicoterapia para distintas acessibilidades",
+      "4- Preparação vocal e musical: vozes, musicalidades e discursos nos processos criativos teatrais",
+      "5- Possibilidades vocais múltiplas para atrizes e atores: do teatro musical à dublagem"
+    ]
+  },
+  {
+    "faculdade": "FALE",
+    "area": "Pedagogia das Artes Cênicas e Práticas Teatrais Contemporâneas",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Possíveis articulações entre teorias e prática no Estágio Supervisionado e a formação artística pedagógica do docente/artista",
+      "2- Artes circenses na educação básica, abordagens possíveis",
+      "3- Perspectiva histórica de teatralidades não hegemônicas: indígena e/ou negra",
+      "4- A pedagogia teatral contemporânea: procedimentos metodológicos e seus objetivos",
+      "5- O ensino de teatro nos espaços formais e não formais da educação"
+    ]
+  },
+  {
+    "faculdade": "FALE",
+    "area": "Linguística e Produção de Textos",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Concepções teóricas de língua, linguagem e gramática",
+      "2- Gêneros discursivos acadêmicos",
+      "3- Teorias linguísticas: estruturalismo, gerativismo, funcionalismo, correntes contemporâneas",
+      "4- Linguagem, sociedade e escola: norma, uso, variação e preconceito linguístico",
+      "5- Linguística Textual: coesão, coerência e textualidade"
+    ]
+  },
+  {
+    "faculdade": "FALE",
+    "area": "Linguística, Língua Portuguesa e Produção de Textos",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Linguística Textual: coesão, coerência e textualidade",
+      "2- Argumentação, retórica e gêneros argumentativos",
+      "3- Gramática da língua portuguesa: fonologia, morfologia, sintaxe, semântica",
+      "4- Gêneros discursivos acadêmicos – fichamento, resumo, resenha, artigo",
+      "5- Metodologia de pesquisa em Letras e elaboração de projetos de pesquisa"
+    ]
+  },
+  {
+    "faculdade": "FALE",
+    "area": "Literatura Brasileira",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- A formação da literatura brasileira e o projeto de nação: do Romantismo ao Modernismo",
+      "2- Modernismo e seus desdobramentos: vanguardas, linguagem e crítica da cultura",
+      "3- Literatura, memória e ditadura no Brasil contemporâneo",
+      "4- Vozes femininas na literatura brasileira do século XXI",
+      "5- Fronteiras da narrativa contemporânea: novas materialidades da literatura"
+    ]
+  },
+  {
+    "faculdade": "FALE",
+    "area": "Literatura e Outros Saberes",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Relação da Literatura com outras linguagens artísticas",
+      "2- Formação estética e crítica de jovens leitores",
+      "3- Representação da diversidade brasileira na literatura infantojuvenil",
+      "4- Intermidialidades contemporâneas e a construção da subjetividade",
+      "5- Novas mídias e narrativas digitais no contexto infantojuvenil"
+    ]
+  },
+  {
+    "faculdade": "FCA",
+    "area": "Zootecnia / Nutrição animal",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Métodos e técnicas de análise para avaliação de alimentos utilizados para animais",
+      "2- Principais alimentos utilizados na alimentação animal",
+      "3- Metabolismo de carboidratos em animais ruminantes",
+      "4- Metabolismo dos compostos nitrogenados em animais ruminantes",
+      "5- Metabolismo e exigência de energia em animais ruminantes"
+    ]
+  },
+  {
+    "faculdade": "FCA",
+    "area": "Construções Rurais e Ambiência",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Técnicas e processos construtivos rurais",
+      "2- Materiais de construção: aglomerados, aglomerantes, argamassas, concretos e materiais cerâmicos",
+      "3- Instrumentação aplicada ao monitoramento ambiental em Instalações Agropecuárias",
+      "4- Modificações ambientais primárias e secundárias",
+      "5- Gerenciamento de obras"
+    ]
+  },
+  {
+    "faculdade": "FCBA",
+    "area": "Planejamento de Processos Biotecnológicos e Bioquímica",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Sistemas integrados de operações relacionadas com a produção de produtos de interesse comercial em biorreatores",
+      "2- Dimensionamento e projetos de biorreatores e plantas de processos biotecnológicos",
+      "3- Planejamento, processos e produção de biocompostos",
+      "4- Desenvolvimento de processos, estudo de produção em bancada, produção industrial “upstream” e ampliação de escala",
+      "5- Purificação de produtos biotecnológicos “downstream” e ampliação de escala"
+    ]
+  },
+  {
+    "faculdade": "FCH",
+    "area": "Geografia Física",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- A bacia hidrográfica como unidade territorial para a gestão de recursos hídricos",
+      "2- Solos arenosos no Brasil: formação, compartimentação morfopedológica, potencialidades e fragilidades ambientais",
+      "3- Geocartografia: sistemas de referências, escala, semiologia gráfica e cartografia escolar",
+      "4- Princípios e conceitos de ecologia aplicados à Biogeografia, a distribuição geográfica dos seres vivos",
+      "5- Climas do Brasil: massas de ar, frentes e zonas de convergências"
+    ]
+  },
+  {
+    "faculdade": "FCH",
+    "area": "Ciências Sociais",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Política: definição e perspectiva",
+      "2- Poder e desigualdade social: classe, raça, gênero e etnia",
+      "3- Mudança social na contemporaneidade: processos e agentes",
+      "4- Cultura, identidade, relações interétnicas e políticas da alteridade",
+      "5- Questão indígena, conflito agrário e territorialidade Guarani e Kaiowá no Mato Grosso do Sul"
+    ]
+  },
+  {
+    "faculdade": "FCH",
+    "area": "Fundamentos de Psicologia, Análise do Comportamento e Psicologia Cognitiva",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- O papel da avaliação e do diagnóstico em Psicologia",
+      "2- Princípios básicos da Análise do Comportamento",
+      "3- Terapia Analítico-comportamental e Terapia Analítico-comportamental Infantil: aspectos teóricos e processo terapêutico",
+      "4- Terapia Cognitiva: aspectos teóricos e processo terapêutico",
+      "5- Questões éticas e desafios atuais"
+    ]
+  },
+  {
+    "faculdade": "FCH",
+    "area": "Psicopatologia e Análise Institucional",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- História da Psicopatologia no Brasil",
+      "2- Psicopatologia e Neurodiversidade",
+      "3- Concepções de Psicopatologia",
+      "4- Neoliberalismo e adoecimento Psíquico",
+      "5- Subjetividade e Discurso"
+    ]
+  },
+  {
+    "faculdade": "FCH",
+    "area": "Psicanálise e Saúde Coletiva",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- A Estrutura e funcionamento do psiquismo",
+      "2- Teoria da técnica psicanalítica: transferência, contratransferência, resistência e interpretação",
+      "3- A clínica psicanalítica na contemporaneidade: cultura, ética e sujeito",
+      "4- Psicanálise e Clínica Ampliada",
+      "5- A neurose e psicose na clínica psicanalítica"
+    ]
+  },
+  {
+    "faculdade": "FCS",
+    "area": "Nutrição em Alimentação Coletiva",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Conceito de Alimentação Coletiva/ O papel do nutricionista na Alimentação Coletiva",
+      "2- UAN (unidades de Alimentação e Nutrição) e UPR (Unidades Produtoras de Refeições): conceitos; características; exigências legais para o seu funcionamento",
+      "3- Localização, layout, setores, ambiência e instalações de uma Unidade Produtora de Refeição (UPR)",
+      "4- Dimensionamento de pessoal; segurança e saúde no trabalho",
+      "5- Gestão de resíduos em Unidades Produtoras de Refeições (UPR). Ações sustentáveis em UPR"
+    ]
+  },
+  {
+    "faculdade": "FCS",
+    "area": "Suporte Básico de Vida",
+    "pontosSorteados": [],
+    "pontos": [
+      "1- Obstrução de vias aéreas",
+      "2- Corrente de Sobrevivência. Reanimação cardiopulmonar do adulto e da criança",
+      "3- Lesões durante prática de esporte na cabeça e coluna vertebral",
+      "4- Primeiros Socorros em Afogamentos",
+      "5- Ferimentos e Choque"
+    ]
+  }
+];
